09c56247809f512297cdc4b6306d947f91401d66
Angelina <angelb511@gmail.com>
2022-10-29 13:25:17 +0100

    removed files

--- week5/config.py
+++
@@ -1,3 +0,0 @@
-config = {
-    'htmlToPdfKey': 'TFA8FGlWwxlAuV2lRRFA8V0aNytm48phxRI9cbqw3bOOsVisSPpOrTDDtaz2PJqj'
-}
--- week5/configGit.py
+++
@@ -1,3 +0,0 @@
-config = {
-    'gitHubToken': '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'
-}
